2e9d76b037c546b13679a0a6b8817f233745279e
robzenhu <robson_150_@hotmail.com>
2022-12-02 22:54:17 -0300

    nao iniciado

---
+++ js/api/KanbanAPI.js
@@ -0,0 +1,77 @@
+export default class kanbanAPI {
+    static getItems(columnId) {
+        const column = read().find(column => column.id == columnId);
+        if (!column) {
+            return [];
+        }
+
+    }
+    static insertItem(columnId, content) {
+        const data = read();
+        const column = data.find(column => column.id == columnId);
+        const item = {
+            id: Math.floor(Math.random() * 100000),
+            content
+        };
+        if (!column) {
+            throw new Error("Coluna não Existe");
+        }
+        column.items.push(item);
+        save(data);
+        return item;
+    }
+    static updateItem(itemId, newProps) {
+        const data = read();
+        const [item, currentColumn] = (() => {
+            for (const column of data) {
+                const item = column.items.find(item => item.id == itemId);
+                if (item) {
+                    return [item, column];
+                }
+            }
+        })();
+        if(!item){
+            throw new Error("Item not found.");
+        }
+        item.content = newProps.content === undefined ? item.content : newProps.content;
+        if(
+            newProps.columnId !== undefined
+            && newProps.position !== undefined
+        ){
+            const targetColumn = data.find(column = column.id == newProps.columnId);
+            if(!targetColumn){
+                throw new Error("target column not found.");
+            }
+            currentColumn.items.splice(currentColumn.items.indexOf(item),1);
+
+            targetColumn.item.splice(newProps.position,0,item);
+        }
+        save(data);
+    }
+
+}
+
+function read() {
+    const json = localStorage.getItem("kanban-data");
+
+    if (!json) {
+        return [
+            {
+                id: 1,
+                items: []
+            },
+            {
+                id: 2,
+                items: []
+            },
+            {
+                id: 3,
+                items: []
+            },
+        ];
+    }
+    return JSON.parse(json);
+}
+function save(data) {
+    localStorage.setItem("kanban-data", JSON.stringify(data));
+}
---
+++ js/main.js
@@ -0,0 +1,6 @@
+import kanbanAPI from "./api/KanbanAPI.js";
+
+kanbanAPI.updateItem(41676,{
+    columnId: 1,
+    position:0
+});
